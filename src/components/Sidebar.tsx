65139df68884b3f8b945f19ba5c56b90c81ab3f9
Yamini Settipalli <139889042+yamini749@users.noreply.github.com>
2025-02-04 08:02:35 +0530

    Update Sidebar.tsx

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -38,7 +38,7 @@ const Sidebar: React.FC<SidebarProps> = ({ isOpen, toggleSidebar }) => {
           <div className="p-6 text-center">
             <div className="w-24 h-24 mx-auto rounded-full overflow-hidden border-2 border-white">
               <img
-                src="public\profile 2.jpg"
+                src="/profile 2.jpg"
                 alt="Yamini Settipalli"
                 className="w-full h-full object-cover"
               />
